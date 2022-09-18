dd8133626ebdef2e63cfffee1b94b9c3eda83914
talhadilber <talhadilber@hipicon.com>
2022-09-18 16:03:28 +0300

    helm upgrade sh added for secret infos

---
+++ ops/helm-upgrade.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Prev Current context => $(kubectl config current-context)"
+kubectl config use-context $ANOUNCY_KUBECTL_NAME
+echo "Next Current context => $(kubectl config current-context)"
+
+cd "/tmp/" && envsubst '$ANOUNCY_DOCKER_HOST,$ANOUNCY_DOCKER_USERNAME,$ANOUNCY_DOCKER_PASSWORD' < "$ANOUNCY_PROJECT_PATH/ops/helm/anouncy/values.yaml" > "values.yaml"
+cd "/tmp/" && helm upgrade --cleanup-on-fail anouncy-1663502774 "$ANOUNCY_PROJECT_PATH/ops/helm/anouncy" --version=0.1.0 --values "values.yaml"
