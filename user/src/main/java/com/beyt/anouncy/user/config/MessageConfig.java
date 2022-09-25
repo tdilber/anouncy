7ff717ea5577f7b4770bf49ca244fb23f06b7d4d
talhadilber <talhadilber@hipicon.com>
2022-09-25 22:10:06 +0300

    I18n Message System created

---
+++ user/src/main/java/com/beyt/anouncy/user/config/MessageConfig.java
@@ -0,0 +1,36 @@
+package com.beyt.anouncy.user.config;
+
+import com.beyt.anouncy.user.UserApplication;
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class MessageConfig {
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        String baseName = environment.getProperty("spring.messages.basename");
+        String encoding = environment.getProperty("spring.messages.encoding");
+
+        if (Strings.isBlank(baseName)) {
+            baseName = "i18n/message";
+        }
+        if (Strings.isBlank(encoding)) {
+            encoding = "UTF-8";
+        }
+
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename(baseName);
+        messageSource.setDefaultEncoding(encoding);
+        messageSource.setBundleClassLoader(UserApplication.class.getClassLoader());
+        messageSource.setAlwaysUseMessageFormat(true);
+        return messageSource;
+    }
+}
---
+++ user/src/main/java/com/beyt/anouncy/user/service/MessageService.java
@@ -0,0 +1,25 @@
+package com.beyt.anouncy.user.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.stereotype.Service;
+
+import java.util.Locale;
+
+@Service
+@Import(ReloadableResourceBundleMessageSource.class)
+public class MessageService {
+
+    @Autowired
+    private MessageSource messageSource;
+
+    public String getMessage(String key) {
+        return getMessage(key, Locale.forLanguageTag("tr"));
+    }
+
+    public String getMessage(String key, Locale locale) {
+        return messageSource.getMessage(key, null, locale);
+    }
+}
