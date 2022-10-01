da0f135b17ef6428bc5b2bdef752fd2e7fd97e2f
talhadilber <talhadilber@hipicon.com>
2022-10-01 14:40:40 +0300

    properties fixes

--- user/src/main/java/com/beyt/anouncy/user/config/AnouncyApplicationProperties.java
+++ user/src/main/java/com/beyt/anouncy/user/config/AnouncyApplicationProperties.java
@@ -1,26 +1,32 @@
 package com.beyt.anouncy.user.config;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@Data
+@Getter
+@Setter
 @ConfigurationProperties(prefix = "anouncy", ignoreUnknownFields = false)
 public class AnouncyApplicationProperties {
-    Password password = new Password();
-    JwtToken jwtToken = new JwtToken();
+    private final Password password = new Password();
+    private final JwtToken jwtToken = new JwtToken();
 
-    @Data
+    @Getter
+    @Setter
     public static class Password {
-        Salt salt = new Salt();
+        private final Salt salt = new Salt();
 
+        @Getter
+        @Setter
         public static class Salt {
-            public String user = null;
-            public String anonymous = null;
-            public String session = null;
+            private String user = null;
+            private String anonymous = null;
+            private String session = null;
         }
     }
 
-    @Data
+    @Getter
+    @Setter
     public static class JwtToken {
         public String secret = null;
     }
