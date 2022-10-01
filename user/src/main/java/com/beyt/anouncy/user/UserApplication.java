437900e00f8835bffcb8cc5f80287c2bf68b5374
talhadilber <talhadilber@hipicon.com>
2022-10-01 14:39:35 +0300

    New Spring Enables added

--- user/src/main/java/com/beyt/anouncy/user/UserApplication.java
+++ user/src/main/java/com/beyt/anouncy/user/UserApplication.java
@@ -4,8 +4,14 @@ import com.beyt.anouncy.user.config.AnouncyApplicationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableAsync
+@EnableScheduling
 @SpringBootApplication
+@EnableAspectJAutoProxy
 @EnableConfigurationProperties({AnouncyApplicationProperties.class})
 public class UserApplication {
 	public static void main(String[] args) {
