64d460dde137a3c29ae16191e7b8f16484a4f66f
talhadilber <talhadilber@hipicon.com>
2022-10-07 22:55:59 +0300

    build-and-publish---single-file-for-all-ci.sh created

--- ops/helm-upgrade.sh
+++ ops/helm-upgrade.sh
@@ -4,5 +4,19 @@ echo "Prev Current context => $(kubectl config current-context)"
 kubectl config use-context $ANOUNCY_KUBECTL_NAME
 echo "Next Current context => $(kubectl config current-context)"
 
-cd "/tmp/" && envsubst '$ANOUNCY_DOCKER_HOST,$ANOUNCY_DOCKER_USERNAME,$ANOUNCY_DOCKER_PASSWORD' < "$ANOUNCY_PROJECT_PATH/ops/helm/anouncy/values.yaml" > "values.yaml"
+source "$ANOUNCY_PROJECT_PATH/ops/helm/anouncy/values-variables.txt"
+export user_deployment_image=$user_deployment_image #TODO maybe we dont have to do this
+echo "---- HELM UPDATE STARTED ----"
+
+cd "/tmp/" && envsubst '$user_deployment_image' < "$ANOUNCY_PROJECT_PATH/ops/helm/anouncy/values.yaml" > "values.yaml"
 cd "/tmp/" && helm upgrade --cleanup-on-fail anouncy-1664399111 "$ANOUNCY_PROJECT_PATH/ops/helm/anouncy" --version=0.1.0 --values "values.yaml"
+
+echo "---- SLEEPING 10 sec ----"
+sleep 10
+kubectl get po
+echo "---- LOGS ----"
+gtimeout 35 kubectl logs -f --tail 50 $(kubectl get pods -o jsonpath='{range .items[*]}{"\n"}{.metadata.name}{"\t"}{range .spec.containers[*]}{.image}{", "}{end}{end}' | grep "$1" | awk '{ print $1 }')
+echo "---- LOGS END ----"
+kubectl get po
+echo "---- HELM UPDATE COMPLETE ----"
+exit 0
--- ops/java-docker-image-builder.sh
+++ ops/java-docker-image-builder.sh
@@ -5,30 +5,36 @@ then
    exit 1
 fi
 
-# TODO skip test close after test written
-cd "$ANOUNCY_PROJECT_PATH/$1/" && mvn clean install package -DskipTests=true
+cd "$ANOUNCY_PROJECT_PATH/backend/$1/" && mvn clean install package
 
 if [[ "$?" -ne 0 ]] ; then
-  echo 'module compile fail'; exit $rc
+  echo 'module compile fail'; exit $?
 fi
 
-baseImageName="anouncy-$1:$(uuidgen)"
-imageName="$ANOUNCY_DOCKER_HOST/$baseImageName"
+if [ -z "$2" ]
+then
+   imageName="$ANOUNCY_DOCKER_HOST/anouncy-$1:$(uuidgen)"
+else
+   imageName="$2"
+fi
+
 docker login $ANOUNCY_DOCKER_HOST -u $ANOUNCY_DOCKER_USERNAME --password-stdin $ANOUNCY_DOCKER_PASSWORD
-echo "Docker Generated Image Name => $imageName"
+echo "Docker Image Name => $imageName"
 echo "---------------------------------------------------"
 echo "Docker Build Starting"
-docker build --platform x86_64 -t $imageName -f "$ANOUNCY_PROJECT_PATH/ops/docker/Dockerfile" "$ANOUNCY_PROJECT_PATH/$1/"
+docker build --platform x86_64 -t $imageName -f "$ANOUNCY_PROJECT_PATH/ops/docker/Dockerfile" "$ANOUNCY_PROJECT_PATH/backend/$1/"
 echo "Docker Build End"
 if [[ "$?" -ne 0 ]] ; then
-  echo 'Docker Build Fail'; exit $rc
+  echo 'Docker Build Fail'; exit $?
 fi
 echo "---------------------------------------------------"
 echo "Docker Push Starting"
 docker push $imageName
 if [[ "$?" -ne 0 ]] ; then
-  echo 'Docker Push Fail'; exit $rc
+  echo 'Docker Push Fail'; exit $?
 fi
 echo "Docker Push End Successfully"
 echo "Docker Pushed Image Name => $imageName"
 echo "---------------------------------------------------"
+
+exit 0
