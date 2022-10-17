9a1ab9c155cfd5749f5dd4dd5e2508c8f8fff51a
talhadilber <talhadilber@hipicon.com>
2022-10-17 23:22:05 +0300

    new module added

---
+++ backend/location/src/main/java/com/beyt/anouncy/location/LocationApplication.java
@@ -0,0 +1,13 @@
+package com.beyt.anouncy.location;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationApplication.class, args);
+	}
+
+}
