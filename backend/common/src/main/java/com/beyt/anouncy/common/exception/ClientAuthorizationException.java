0abefd285d42aa8ada364a791117b31a6f833f47
talhadilber <talhadilber@hipicon.com>
2022-11-06 10:21:47 +0300

    small refactors

--- backend/common/src/main/java/com/beyt/anouncy/common/exception/ClientAuthorizationException.java
+++ backend/common/src/main/java/com/beyt/anouncy/common/exception/ClientAuthorizationException.java
@@ -1,30 +1,18 @@
 package com.beyt.anouncy.common.exception;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class ClientAuthorizationException extends RuntimeException {
     private List<String> errorMessageKeyList;
 
-    public ClientAuthorizationException(String... errorMessageKey) {
-        super(String.join(", ", errorMessageKey));
-        errorMessageKeyList = Arrays.stream(errorMessageKey).toList();
-    }
-
-    public ClientAuthorizationException(List<String> errorMessageKey) {
-        super(String.join(", ", errorMessageKey));
-        errorMessageKeyList = errorMessageKey;
-    }
-
     public ClientAuthorizationException(String errorMessageKey) {
         super(errorMessageKey);
-        errorMessageKeyList = Collections.singletonList(errorMessageKey);
+        errorMessageKeyList = List.of(errorMessageKey);
     }
 
     public ClientAuthorizationException(String errorMessageKey, Throwable cause) {
         super(errorMessageKey, cause);
-        errorMessageKeyList = Collections.singletonList(errorMessageKey);
+        errorMessageKeyList = List.of(errorMessageKey);
     }
 
     public List<String> getErrorMessageKeyList() {
--- backend/common/src/main/java/com/beyt/anouncy/common/exception/ClientErrorException.java
+++ backend/common/src/main/java/com/beyt/anouncy/common/exception/ClientErrorException.java
@@ -1,30 +1,18 @@
 package com.beyt.anouncy.common.exception;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class ClientErrorException extends RuntimeException {
     private List<String> errorMessageKeyList;
 
-    public ClientErrorException(String... errorMessageKey) {
-        super(String.join(", ", errorMessageKey));
-        errorMessageKeyList = Arrays.stream(errorMessageKey).toList();
-    }
-
-    public ClientErrorException(List<String> errorMessageKey) {
-        super(String.join(", ", errorMessageKey));
-        errorMessageKeyList = errorMessageKey;
-    }
-
     public ClientErrorException(String errorMessageKey) {
         super(errorMessageKey);
-        errorMessageKeyList = Collections.singletonList(errorMessageKey);
+        errorMessageKeyList = List.of(errorMessageKey);
     }
 
     public ClientErrorException(String errorMessageKey, Throwable cause) {
         super(errorMessageKey, cause);
-        errorMessageKeyList = Collections.singletonList(errorMessageKey);
+        errorMessageKeyList = List.of(errorMessageKey);
     }
 
     public List<String> getErrorMessageKeyList() {
