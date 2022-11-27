c8ae8d322990469dac62e1fa2f421ee570acbb92
talhadilber <talhadilber@hipicon.com>
2022-11-27 19:17:45 +0300

    Database Configuration completed

---
+++ backend/announce-listing/src/main/java/com/beyt/anouncy/listing/config/DatabaseConfiguration.java
@@ -0,0 +1,27 @@
+package com.beyt.anouncy.listing.config;
+
+import org.neo4j.driver.Driver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.neo4j.core.DatabaseSelectionProvider;
+import org.springframework.data.neo4j.core.transaction.Neo4jTransactionManager;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+import org.springframework.data.neo4j.repository.config.Neo4jRepositoryConfigurationExtension;
+import org.springframework.transaction.TransactionManager;
+
+@Configuration
+@EnableNeo4jRepositories(
+        basePackages = "com.beyt.anouncy",
+        includeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = Neo4jRepository.class)
+)
+//@EnableElasticsearchRepositories("com.beyt.repository.search")
+public class DatabaseConfiguration {
+
+    @Bean(Neo4jRepositoryConfigurationExtension.DEFAULT_TRANSACTION_MANAGER_BEAN_NAME)
+    public TransactionManager transactionManager(Driver driver, DatabaseSelectionProvider databaseNameProvider) {
+        return new Neo4jTransactionManager(driver, databaseNameProvider);
+    }
+}
