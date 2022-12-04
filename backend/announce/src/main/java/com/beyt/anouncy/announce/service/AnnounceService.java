0a2a6d3113b2b0f296fa9b39e4d568f93968e79d
talhadilber <talhadilber@hipicon.com>
2022-12-04 14:01:03 +0300

    small fixes

--- backend/announce/src/main/java/com/beyt/anouncy/announce/service/AnnounceService.java
+++ backend/announce/src/main/java/com/beyt/anouncy/announce/service/AnnounceService.java
@@ -4,6 +4,7 @@ import com.beyt.anouncy.announce.dto.AnnounceCreateDTO;
 import com.beyt.anouncy.common.aspect.NeedLogin;
 import com.beyt.anouncy.common.context.UserContext;
 import com.beyt.anouncy.common.entity.neo4j.Announce;
+import com.beyt.anouncy.common.entity.neo4j.AnonymousUser;
 import com.beyt.anouncy.common.entity.neo4j.Region;
 import com.beyt.anouncy.common.repository.AnnounceRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,13 +21,13 @@ public class AnnounceService {
     @NeedLogin
     public void receiveAnnounce(AnnounceCreateDTO dto) {
         Announce announce = new Announce();
-        announce.setAnonymousUserId(userContext.getAnonymousUserId());
+        announce.setAnonymousUser(new AnonymousUser(userContext.getAnonymousUserId())); // TODO im not sure
         announce.setBody(dto.getBody());
         announce.setBeginRegion(new Region());
         announce.setCurrentRegion(new Region());
     }
-
-    public Region getRelatedRegion() {
-
-    }
+//
+//    public Region getRelatedRegion() {
+//
+//    }
 }
