327e96417040e030823782618a5bb17f7392e91d
talhadilber <talhadilber@hipicon.com>
2022-12-15 20:54:16 +0300

    Location Service sql migration codes removed

--- backend/location/src/main/java/com/beyt/anouncy/location/service/LocationService.java
+++ backend/location/src/main/java/com/beyt/anouncy/location/service/LocationService.java
@@ -1,26 +1,18 @@
 package com.beyt.anouncy.location.service;
 
-import com.beyt.anouncy.common.entity.enumeration.LocationStatus;
-import com.beyt.anouncy.common.entity.enumeration.LocationType;
 import com.beyt.anouncy.location.dto.LocationDTO;
 import com.beyt.anouncy.location.entity.Location;
 import com.beyt.anouncy.location.model.GeoJsonItem;
 import com.beyt.anouncy.location.repository.LocationRepository;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
-import org.apache.logging.log4j.util.Strings;
 import org.bson.BsonArray;
-import org.bson.Document;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
 
-import javax.sql.DataSource;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
@@ -28,10 +20,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.text.Normalizer;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.regex.Pattern;
@@ -48,27 +37,9 @@ public class LocationService {
     @Autowired
     private LocationRepository locationRepository;
 
-    private final JdbcTemplate jdbcTemplate;
-
-    public LocationService(DataSource dataSource) {
-        jdbcTemplate = new JdbcTemplate();
-        jdbcTemplate.setDataSource(dataSource);
-    }
-
     public LocationDTO findAllByParentId(Long parentId) {
         List<Location> locationList = locationRepository.findAllByParentLocationIdIsIn(List.of(parentId));
         return new LocationDTO(locationList);
-    }
-
-    @SneakyThrows
-    public void fetchAllAndSaveMongo() {
-        List<Location> locationList = getLocations();
-
-        fillGeometryTurkey(locationList);
-        fillGeometryCity(locationList);
-        fillGeometryDistinct(locationList);
-        System.out.println(locationList);
-//        locationRepository.saveAll(locationList);
     }
 
     private void fillGeometryTurkey(List<Location> locationList) throws IOException {
@@ -134,32 +105,6 @@ public class LocationService {
             }
 
         }
-    }
-
-    private List<Location> getLocations() {
-        List<Location> locationList = jdbcTemplate.query("SELECT * FROM locations ", new RowMapper<Location>() {
-            @Override
-            public Location mapRow(ResultSet rs, int rownumber) throws SQLException {
-                Location location = new Location();
-                location.setId(rs.getLong("id"));
-                location.setName(rs.getString("name"));
-                location.setOrdinal(rs.getInt("ordinal"));
-                location.setUserUsage(rs.getInt("user_usage"));
-                location.setLatitude(rs.getDouble("latitude"));
-                location.setLongitude(rs.getDouble("longitude"));
-                int type = rs.getInt("type");
-                int status = rs.getInt("status");
-                location.setType(Arrays.stream(LocationType.values()).filter(v -> v.getValue() == type).findFirst().orElse(null));
-                location.setStatus(Arrays.stream(LocationStatus.values()).filter(v -> v.getValue() == status).findFirst().orElse(null));
-                location.setParentLocationId(rs.getLong("parent_location_id"));
-                String googleMapApiInfo = rs.getString("google_map_api_info");
-                location.setGoogleMapApiInfo(Strings.isNotBlank(googleMapApiInfo) ? Document.parse(googleMapApiInfo) : null);
-                location.setPath(rs.getString("path"));
-
-                return location;
-            }
-        });
-        return locationList;
     }
 
     public Location getLatLonFromGoogleMap(String locationAddress) {
