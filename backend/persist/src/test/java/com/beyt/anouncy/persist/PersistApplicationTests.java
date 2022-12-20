09823740501a57c2508d06106b8bec1a4c25b3e3
talhadilber <talhadilber@hipicon.com>
2022-12-20 20:01:58 +0300

    Grpc Client runtime reflection collection example

--- backend/persist/src/test/java/com/beyt/anouncy/persist/PersistApplicationTests.java
+++ backend/persist/src/test/java/com/beyt/anouncy/persist/PersistApplicationTests.java
@@ -1,12 +1,21 @@
 package com.beyt.anouncy.persist;
 
-import com.beyt.anouncy.common.persist.Count;
-import com.beyt.anouncy.common.persist.Empty;
-import com.beyt.anouncy.common.persist.VotePersistServiceGrpc;
+
+import com.beyt.anouncy.common.persist.v1.VotePersistServiceGrpc;
+import com.beyt.anouncy.common.v1.Count;
+import com.beyt.anouncy.common.v1.Empty;
+import io.grpc.Channel;
+import net.devh.boot.grpc.client.channelfactory.GrpcChannelFactory;
 import net.devh.boot.grpc.client.inject.GrpcClient;
+import net.devh.boot.grpc.client.inject.GrpcClientBeanPostProcessor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Member;
+import java.lang.reflect.Method;
 
 //@SpringBootTest(classes = PersistApplication.class, properties = {
 //		"grpc.server.inProcessName=test", // Enable inProcess server
@@ -24,11 +33,28 @@ class PersistApplicationTests {
 	@GrpcClient("persist-grpc-server")
 	private VotePersistServiceGrpc.VotePersistServiceBlockingStub votePersistServiceBlockingStub;
 
+	@Autowired
+	private GrpcChannelFactory grpcChannelFactory;
+	@Autowired
+	private GrpcClientBeanPostProcessor postProcessor;
+
 	@Test
 //	@DirtiesContext
-	void contextLoads() {
+	void contextLoads() throws Throwable {
 //		VotePersistServiceGrpc.VotePersistServiceBlockingStub mock = Mockito.mock(VotePersistServiceGrpc.VotePersistServiceBlockingStub.class);
 		Count count = votePersistServiceBlockingStub.count(Empty.newBuilder().build());
+		VotePersistServiceGrpc.VotePersistServiceBlockingStub stub = getGrpcClient("persist-grpc-server", VotePersistServiceGrpc.VotePersistServiceBlockingStub.class);
+		System.out.println(stub);
 	}
+
+	private <T> T getGrpcClient(String name, Class<T> clazz) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+		Channel channel = grpcChannelFactory.createChannel(name);
+		Method method = postProcessor.getClass().getDeclaredMethod("valueForMember", String.class, Member.class, Class.class, Channel.class);
+
+		method.setAccessible(true);
+		T stub = (T) method.invoke(postProcessor, name, null, clazz, channel);
+		return stub;
+	}
+
 
 }
