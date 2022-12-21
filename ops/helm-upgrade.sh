2d61f31a76e1b8c6e9a54ccdfd587c730b52564b
talhadilber <talhadilber@hipicon.com>
2022-12-21 21:41:46 +0300

    Mongo helm chart added

--- ops/helm-upgrade.sh
+++ ops/helm-upgrade.sh
@@ -4,7 +4,6 @@ echo "Prev Current context => $(kubectl config current-context)"
 kubectl config use-context $ANOUNCY_KUBECTL_NAME
 echo "Next Current context => $(kubectl config current-context)"
 
-
 source "$ANOUNCY_PROJECT_PATH/ops/helm/anouncy/values-variables.txt"
 #TODO maybe we dont have to do this
 export user_deployment_image=$user_deployment_image
@@ -13,10 +12,16 @@ export listing_deployment_image=$listing_deployment_image
 export vote_deployment_image=$vote_deployment_image
 export region_deployment_image=$region_deployment_image
 export persist_deployment_image=$persist_deployment_image
+export search_deployment_image=$search_deployment_image
 export location_deployment_image=$location_deployment_image
+export mongodb_username=$mongodb_username
+export mongodb_password=$mongodb_password
+export elasticsearch_username=$elasticsearch_username
+export elasticsearch_password=$elasticsearch_password
+export neo4j_password=$neo4j_password
 echo "---- HELM UPDATE STARTED ----"
 
-cd "/tmp/" && envsubst '$user_deployment_image,$announce_deployment_image,$listing_deployment_image,$vote_deployment_image,$region_deployment_image,$persist_deployment_image,$location_deployment_image' < "$ANOUNCY_PROJECT_PATH/ops/helm/anouncy/values.yaml" > "values.yaml"
+cd "/tmp/" && envsubst '$user_deployment_image,$announce_deployment_image,$listing_deployment_image,$vote_deployment_image,$region_deployment_image,$persist_deployment_image,$location_deployment_image,$search_deployment_image,$mongodb_username,$mongodb_password,$elasticsearch_username,$elasticsearch_password,$neo4j_password' < "$ANOUNCY_PROJECT_PATH/ops/helm/anouncy/values.yaml" > "values.yaml"
 cd "/tmp/" && helm upgrade --cleanup-on-fail anouncy-1664399111 "$ANOUNCY_PROJECT_PATH/ops/helm/anouncy" --version=0.1.0 --values "values.yaml"
 
 echo "---- SLEEPING 10 sec ----"
--- ops/helm/persistance-helm-install.sh
+++ ops/helm/persistance-helm-install.sh
@@ -5,3 +5,7 @@ helm install prod-postgresql -n persist bitnami/postgresql --version 11.9.5 --va
 kubectl apply -n persist -f "$ANOUNCY_PROJECT_PATH/ops/helm/neo4j/neo4j-password-secret.yaml"
 helm repo add equinor-charts https://equinor.github.io/helm-charts/charts/
 helm install prod-neo4j  -n persist equinor-charts/neo4j-community --version 1.2.5 --values "$ANOUNCY_PROJECT_PATH/ops/helm/neo4j/values.yaml"
+
+kubectl apply -n persist -f "$ANOUNCY_PROJECT_PATH/ops/helm/mongo/mongo-password-secret.yaml"
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm install prod-mongodb  -n persist bitnami/mongodb --version 13.6.2 --values "$ANOUNCY_PROJECT_PATH/ops/helm/mongo/values.yaml"
